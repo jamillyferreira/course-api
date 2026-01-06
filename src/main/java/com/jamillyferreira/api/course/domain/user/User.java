315b890ee3eee1abbda44b8c302a90d04ad14574
Jamilly Ferreira <jamillyferreira039@gmail.com>
2026-01-06 10:20:44 -0400

    feat: cria entidade User

---
+++ src/main/java/com/jamillyferreira/api/course/domain/user/User.java
@@ -0,0 +1,41 @@
+package com.jamillyferreira.api.course.domain.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Objects;
+
+
+@Entity
+@Table(name = "tb_user")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    // usamos IDENTITY para deixar o banco gerar o id e evitar problemas com sequência em MySQL/H2
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+    private String phone;
+    private String password;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
