ac16a80ed24a1a95a500a5a775ce88a2c198e138
Jamilly Ferreira <jamillyferreira039@gmail.com>
2026-01-06 11:52:48 -0400

    feat: cria UserRepository e TestConfig para carga de dados de teste

---
+++ src/main/java/com/jamillyferreira/api/course/config/TestConfig.java
@@ -0,0 +1,31 @@
+package com.jamillyferreira.api.course.config;
+
+import com.jamillyferreira.api.course.domain.user.User;
+import com.jamillyferreira.api.course.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.logging.Logger;
+
+@Configuration
+@Profile("test")
+public class TestConfig implements CommandLineRunner {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
+        User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
+
+        userRepository.saveAll(List.of(u1, u2));
+
+    }
+}
---
+++ src/main/java/com/jamillyferreira/api/course/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.jamillyferreira.api.course.repository;
+
+import com.jamillyferreira.api.course.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
