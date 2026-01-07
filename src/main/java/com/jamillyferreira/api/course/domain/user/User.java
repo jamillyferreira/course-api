c5ea1a82d21b7080f816ec63e5f2b2deebe164bd
Jamilly Ferreira <jamillyferreira039@gmail.com>
2026-01-07 15:43:36 -0400

    refactor: ajusta serialização JSON e construtores das entidades

--- src/main/java/com/jamillyferreira/api/course/domain/user/User.java
+++ src/main/java/com/jamillyferreira/api/course/domain/user/User.java
@@ -1,12 +1,13 @@
 package com.jamillyferreira.api.course.domain.user;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.jamillyferreira.api.course.domain.order.Order;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 
@@ -21,11 +22,22 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String name;
     private String email;
     private String phone;
     private String password;
+
+    @JsonIgnore
+    @Setter(AccessLevel.NONE)
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
+
+    public User(Long id, String name, String email, String phone, String password) {
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.password = password;
+    }
 
     @Override
     public boolean equals(Object o) {
