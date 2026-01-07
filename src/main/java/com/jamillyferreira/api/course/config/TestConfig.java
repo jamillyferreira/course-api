bad9d2b64806ea22e2ecf13be8e67199085e3c6a
Jamilly Ferreira <jamillyferreira039@gmail.com>
2026-01-07 15:39:05 -0400

    test: adiciona dados iniciais de Category e Order

--- src/main/java/com/jamillyferreira/api/course/config/TestConfig.java
+++ src/main/java/com/jamillyferreira/api/course/config/TestConfig.java
@@ -1,17 +1,18 @@
 package com.jamillyferreira.api.course.config;
 
+import com.jamillyferreira.api.course.domain.category.Category;
+import com.jamillyferreira.api.course.domain.order.Order;
 import com.jamillyferreira.api.course.domain.user.User;
+import com.jamillyferreira.api.course.repository.CategoryRepository;
+import com.jamillyferreira.api.course.repository.OrderRepository;
 import com.jamillyferreira.api.course.repository.UserRepository;
-import lombok.extern.slf4j.Slf4j;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
-import java.util.Arrays;
+import java.time.Instant;
 import java.util.List;
-import java.util.logging.Logger;
 
 @Configuration
 @Profile("test")
@@ -19,13 +20,29 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
     @Override
     public void run(String... args) throws Exception {
+
+        Category cat1 = new Category(null, "Electronics");
+        Category cat2 = new Category(null, "Books");
+        Category cat3 = new Category(null, "Computers");
 
         User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
         User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 
+        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
+        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
+        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+
         userRepository.saveAll(List.of(u1, u2));
+        orderRepository.saveAll(List.of(o1, o2, o3));
+        categoryRepository.saveAll(List.of(cat1, cat2, cat3));
 
     }
 }
