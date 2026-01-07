cfc74bd405a34b33603b7441b16208870c41edfd
Jamilly Ferreira <jamillyferreira039@gmail.com>
2026-01-07 18:58:07 -0400

    fix: adiciona os status no pedido para teste

--- src/main/java/com/jamillyferreira/api/course/config/TestConfig.java
+++ src/main/java/com/jamillyferreira/api/course/config/TestConfig.java
@@ -2,6 +2,7 @@ package com.jamillyferreira.api.course.config;
 
 import com.jamillyferreira.api.course.domain.category.Category;
 import com.jamillyferreira.api.course.domain.order.Order;
+import com.jamillyferreira.api.course.domain.order.OrderStatus;
 import com.jamillyferreira.api.course.domain.user.User;
 import com.jamillyferreira.api.course.repository.CategoryRepository;
 import com.jamillyferreira.api.course.repository.OrderRepository;
@@ -36,9 +37,9 @@ public class TestConfig implements CommandLineRunner {
         User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
         User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 
-        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.CANCELED, u1);
+        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.DELIVERED, u2);
+        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.SHIPPED, u1);
 
         userRepository.saveAll(List.of(u1, u2));
         orderRepository.saveAll(List.of(o1, o2, o3));
