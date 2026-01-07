591d9d8445bef67cc60b30df29be672dd9701fa2
Jamilly Ferreira <jamillyferreira039@gmail.com>
2026-01-07 19:19:00 -0400

    feat: implementa métodos

--- src/main/java/com/jamillyferreira/api/course/service/CategoryService.java
+++ src/main/java/com/jamillyferreira/api/course/service/CategoryService.java
@@ -1,10 +1,24 @@
 package com.jamillyferreira.api.course.service;
 
+import com.jamillyferreira.api.course.domain.category.Category;
+import com.jamillyferreira.api.course.repository.CategoryRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CategoryService {
     @Autowired
-    private CategoryService service;
+    private CategoryRepository repository;
+
+    public List<Category> findAll() {
+        return repository.findAll();
+    }
+
+    public Category findById(Long id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Categoria não encontrada!"));
+    }
 }
