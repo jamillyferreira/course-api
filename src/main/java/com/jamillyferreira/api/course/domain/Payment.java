7b3509671d4e508d4a9161c57cc407efedf02d57
Jamilly Ferreira <jamillyferreira039@gmail.com>
2026-01-18 13:48:27 -0400

    feat: Implementa Payment

---
+++ src/main/java/com/jamillyferreira/api/course/domain/Payment.java
@@ -0,0 +1,42 @@
+package com.jamillyferreira.api.course.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.Instant;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_payment")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Payment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Instant moment;
+
+    @JsonIgnore
+    @OneToOne
+    @MapsId // Indica que usa mesmo ID de Order (entidade principal)
+    private Order order;
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Payment payment = (Payment) o;
+        return Objects.equals(id, payment.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
